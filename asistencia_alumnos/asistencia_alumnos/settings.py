7da2796385364d4a1004729aa207dc3b06194807
unknown <ibanezlucas@chaco.gob.ar>
2024-04-29 15:41:49 -0300

    pagina inicial de home y login

--- asistencia_alumnos/asistencia_alumnos/settings.py
+++ asistencia_alumnos/asistencia_alumnos/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'asistencia_alumnos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
