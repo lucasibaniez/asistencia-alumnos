5f5fbeb9000a9a47718d1d4f1d733d3651e050b6
unknown <ibanezlucas@chaco.gob.ar>
2024-05-06 15:52:37 -0300

    creamos tabla clase

--- asistencia_alumnos/apps/clases/models.py
+++ asistencia_alumnos/apps/clases/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Clase(models.Model):
+    fecha = models.DateField()
+
+
+
+
+"""
+class NuevaTabla(models.Model):
+    pass
+
+    class Meta:
+        db_table = 'nueva_tabla'
+"""
