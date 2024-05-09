fb034996deccf23f742a35f969c27264f62eb56b
unknown <ibanezlucas@chaco.gob.ar>
2024-05-09 15:17:15 -0300

    se creo funcionalidad para iniciar sesion

--- asistencia_alumnos/asistencia_alumnos/views.py
+++ asistencia_alumnos/asistencia_alumnos/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib.auth import authenticate, login as login_django
+
 
 from apps.usuarios.models import Usuario
 
 def login(request):
-    return render(request, 'login.html', {})
+    nombre_usuario = ""
+    mensaje_error = None
+    if request.method == "POST":
+        nombre_usuario = request.POST.get("username") # request.GET.get("username")
+        contrasenia = request.POST.get("password") # request.GET.get("password")
+        u = authenticate(request, username=nombre_usuario, password=contrasenia)
+
+        if u:
+            login_django(request, u)
+
+            return redirect("pagina_principal")
+        else:
+            mensaje_error = "Usuario y/o contraseña incorrecta"
+
+    ctx = {
+        "nombre_usuario": nombre_usuario,
+        "mensaje_error": mensaje_error
+    }
+    return render(request, 'login.html', ctx)
 
 def home(request):
     template_name = 'home.html'
