30d405b05531bb79e9fcd9e0efd3ae9896c89242
unknown <ibanezlucas@chaco.gob.ar>
2024-05-16 14:28:01 -0300

    listado de clases

--- asistencia_alumnos/apps/clases/views.py
+++ asistencia_alumnos/apps/clases/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from .models import Clase
+
 def listar_clases(request):
     template_name = 'clases/lista.html'
+
+    
+
     ctx = {
+        "clases": Clase.objects.all().order_by("-fecha")
     }
     return render(request, template_name, ctx)
